6e7b6af6ffa978e2d7d2c4176272bd08e6eb885f
f_nosrati <f_nosrati@SHL-62>
2023-11-29 20:36:38 +0330

    -- refactor solution

---
+++ src/Application/Features/Products/Commands/ProductCommands/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace WebApplicationMediatR.Application.Features.Products.Commands.ProductCommands.CreateProduct;
+
+public class CreateProductCommand : IRequest
+{
+    public string ProductTitle { get; set; }
+
+    public string ProductName { get; set; }
+}
---
+++ src/Application/Features/Products/Queries/Products/GetProductById/GetProductByIdQueryHandler.cs
@@ -0,0 +1,46 @@
+﻿using MediatR;
+using WebApplicationMediatR.Domain.Entity;
+using WebApplicationMediatR.Domain;
+using WebApplicationMediatR.ViewModels.ProductViewModels;
+
+namespace WebApplicationMediatR.Application.Features.Products.Queries.Products.GetProductById;
+
+public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, ProductVM>
+{
+    #region Fields
+
+    private readonly IProductRepository _productService;
+    #endregion Fields
+
+    #region Ctor
+
+    public GetProductByIdQueryHandler(IProductRepository productService)
+    {
+        _productService = productService;
+    }
+
+    #endregion Ctor
+
+    #region Handle
+
+
+    public async Task<ProductVM> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+    {
+        var product = await _productService.GetProduct(request.ProductId, cancellationToken).ConfigureAwait(false);
+
+        var productVM = CreateProductVM(product);
+
+        return productVM;
+    }
+
+    #endregion
+
+    #region Private
+
+    private ProductVM CreateProductVM(Product products)
+        => new ProductVM();
+
+
+    #endregion Private
+
+}
